a78a5e98c94ad01ec6b67ec20fbcfaefb5c38c9c
U-WNEWEB\19001130 <19001130@T1-1-1-Q-37555.wneweb.com.tw>
2021-05-05 16:48:57 +0800

    implement 3 fans setting option, need to test on FAN BOARD

--- truck/shell/Artesyn_setting_adj.sh
+++ truck/shell/Artesyn_setting_adj.sh
@@ -5,33 +5,52 @@ helpFunction()
 {
     echo ""
     echo "Usage: $0 -s parameterB"
-    echo -e "\t-s Description of setting/reading the Artesyn LGA80D, 1: for reading, 2: for setting"
+    echo -e "\t-s Description of setting/reading the power module, 1: for reading, 2: for setting, 3: no operation"
+    echo -e "\t-d Description of which device you want to read/set, 1: for Renesas, 2: for Artesyn"
+    echo -e "\t-r Description of which device you want to restore_factory, 1: reset as factory, 2: no operation"
     exit 1 # Exit script after printing help
 }
 
-while getopts "s:" opt
+while getopts "s:d:r:" opt
 do
     case "$opt" in
         s ) option="$OPTARG";;
+        d ) device="$OPTARG";;
+        r ) reset="$OPTARG";;
         ? ) helpfunction;; #Print helpFunction in case parameter is non-existent
     esac
 done
 
 # Print helpFunction in case parameter are empty
-if [ -z $option ] || [ $option -lt 1 ] || [ $option -gt 2 ]
+if [ -z $option ] || [ -z $device ] || [ -z $reset ] || [ $option -lt 1 ] || [ $option -gt 3 ] || [ $device -lt 1 ] || [ $device -gt 2 ] || [ $reset -lt 1 ] || [ $reset -gt 2 ]
+
 then
-    echo "The option should be 1 or 2";
+    echo "The option read/write should be 1, 2,or 3";
+    echo "The device selection should be 1 or 2";
+    echo "The reset selection should be 1 or 2";
     helpFunction
 fi
 
 
 echo "Totalargument: $#"
-echo "Option is: $target (1 for reading, 2 for setting)"
+echo "Option is: $option (1 for reading, 2 for setting, 3 for no opweration)"
+echo "Device is: $device (1 for Renesas, 2 for Artesyn)"
+echo "Reset is: $reset (1 for reset to factory, 2 no operation)"
 
 i2cset_80D="i2cset -f -y 1"
 i2cget_80D="i2cget -f -y 1"
+if [ $device -eq 2 ]
+then
 master="0x43"
 slave="0x42"
+elif [ $device -eq 1 ]
+then
+master="0x19"
+slave="0x1a"
+fi
+r_RESTORE_FACTORY="0xF4"
+r_RESTORE_USER_ALL="0x16"
+r_STORE_USER_ALL="0x15"
 r_PAGE="0x00"
 r_FREQUENCY_SWITCH="0x33"           #2bytes
 r_VIN_UV_FAULT_LIMIT="0x59"         #2bytes
@@ -58,9 +77,32 @@ r_INTERLEAVE="0x37"                 #2bytes
 r_VOUT_DROOP="0x28"                 #2bytes
 r_MULTI_PHASE_RAMP_GAIN="0xd5"      #1byte
 
-h_PAGE_0="0x00"
-h_PAGE_1="0x01"
-h_PAGE_all="0xff"
+h_FREQUENCY_SWITCH="0xFB92"           #2bytes
+h_VIN_UV_FAULT_LIMIT="0xCB10"         #2bytes
+h_VIN_UV_WARN_LIMIT="0xCB5E"          #2bytes
+h_VIN_OV_FAULT_LIMIT="0xD393"         #2bytes
+h_VIN_OV_WARN_LIMIT="0xD360"          #2bytes
+h_OT_FAULT_LIMIT="0xEBE8"             #2bytes
+h_OT_WARN_LIMIT="0xEB70"              #2bytes
+h_VOUT_OV_FAULT_LIMIT="0x2333"        #2bytes
+h_IOUT_AVG_OC_FAULT_LIMIT="0xE2D0"    #2bytes
+h_IOUT_OC_FAULT_LIMIT="0xE320"        #2bytes
+h_IOUT_CAL_GAIN="0xB376"              #2bytes
+h_IOUT_CAL_OFFSET="0xB533"            #2bytes
+h_IOUT_UC_FAULT_LIMIT="0xEDD0"        #2bytes
+h_ISENSE_CONFIG="0x320E"              #2bytes
+h_IOUT_AVG_UC_FAULT_LIMIT="0xE4E0"    #2bytes
+h_VOUT_COMMAND="0x2000"               #2bytes
+h_VOUT_MAX="0x22F6"                   #2bytes
+h_VOUT_UV_FAULT_LIMIT="0x1B34"        #2bytes
+h_VOUT_MARGIN_HIGH="0x2199"           #2bytes
+h_VOUT_MARGIN_LOW="0x1E67"            #2bytes
+h_POWER_GOOD_ON="0x1CCD"              #2bytes
+h_INTERLEAVE="0x0000"                 #2bytes
+h_VOUT_DROOP="0x9A3D"                 #2bytes
+h_MULTI_PHASE_RAMP_GAIN="0x03"        #1byte
+
+
 if [ $option -eq 1 ]
 then
     for i in {0..1}
@@ -75,42 +117,96 @@ then
         for j in {0..1}
         do
             echo "$i2cset_80D $addr $r_PAGE $j"
-            echo "$i2cget_80D $addr $r_FREQUENCY_SWITCH w"       
-            echo "$i2cget_80D $addr $r_VIN_UV_FAULT_LIMIT w"     
-            echo "$i2cget_80D $addr $r_VIN_UV_WARN_LIMIT w"      
-            echo "$i2cget_80D $addr $r_VIN_OV_FAULT_LIMIT w"     
-            echo "$i2cget_80D $addr $r_VIN_OV_WARN_LIMIT w"      
-            echo "$i2cget_80D $addr $r_OT_FAULT_LIMIT w"         
-            echo "$i2cget_80D $addr $r_OT_WARN_LIMIT w"          
-            echo "$i2cget_80D $addr $r_VOUT_OV_FAULT_LIMIT w"    
-            echo "$i2cget_80D $addr $r_IOUT_AVG_OC_FAULT_LIMIT w"
-            echo "$i2cget_80D $addr $r_IOUT_OC_FAULT_LIMIT w"    
-            echo "$i2cget_80D $addr $r_IOUT_CAL_GAIN w"          
-            echo "$i2cget_80D $addr $r_IOUT_CAL_OFFSET w"        
-            echo "$i2cget_80D $addr $r_IOUT_UC_FAULT_LIMIT w"    
-            echo "$i2cget_80D $addr $r_ISENSE_CONFIG w"          
-            echo "$i2cget_80D $addr $r_IOUT_AVG_UC_FAULT_LIMIT w"
-            echo "$i2cget_80D $addr $r_VOUT_COMMAND w"      
-            echo "$i2cget_80D $addr $r_VOUT_MAX w"               
-            echo "$i2cget_80D $addr $r_VOUT_UV_FAULT_LIMIT w"
-            echo "$i2cget_80D $addr $r_VOUT_MARGIN_HIGH w"
-            echo "$i2cget_80D $addr $r_VOUT_MARGIN_LOW w"
-            echo "$i2cget_80D $addr $r_POWER_GOOD_ON w"
-            echo "$i2cget_80D $addr $r_INTERLEAVE w"
-            echo "$i2cget_80D $addr $r_VOUT_DROOP w"
-            echo "$i2cget_80D $addr $r_MULTI_PHASE_RAMP_GAIN"
+            echo "FREQUENCY_SWITCH ,        $($i2cget_80D $addr $r_FREQUENCY_SWITCH w)"       
+            echo "VIN_UV_FAULT_LIMIT ,      $($i2cget_80D $addr $r_VIN_UV_FAULT_LIMIT w)"     
+            echo "VIN_UV_WARN_LIMIT ,       $($i2cget_80D $addr $r_VIN_UV_WARN_LIMIT w)"      
+            echo "VIN_OV_FAULT_LIMIT ,      $($i2cget_80D $addr $r_VIN_OV_FAULT_LIMIT w)"     
+            echo "VIN_OV_WARN_LIMIT ,       $($i2cget_80D $addr $r_VIN_OV_WARN_LIMIT w)"      
+            echo "OT_FAULT_LIMIT ,          $($i2cget_80D $addr $r_OT_FAULT_LIMIT w)"         
+            echo "OT_WARN_LIMIT ,           $($i2cget_80D $addr $r_OT_WARN_LIMIT w)"          
+            echo "VOUT_OV_FAULT_LIMIT ,     $($i2cget_80D $addr $r_VOUT_OV_FAULT_LIMIT w)"    
+            echo "IOUT_AVG_OC_FAULT_LIMIT,  $($i2cget_80D $addr $r_IOUT_AVG_OC_FAULT_LIMIT w)"
+            echo "IOUT_OC_FAULT_LIMIT ,     $($i2cget_80D $addr $r_IOUT_OC_FAULT_LIMIT w)"    
+            echo "IOUT_CAL_GAIN ,           $($i2cget_80D $addr $r_IOUT_CAL_GAIN w)"          
+            echo "IOUT_CAL_OFFSET ,         $($i2cget_80D $addr $r_IOUT_CAL_OFFSET w)"        
+            echo "IOUT_UC_FAULT_LIMIT ,     $($i2cget_80D $addr $r_IOUT_UC_FAULT_LIMIT w)"    
+            echo "ISENSE_CONFIG ,           $($i2cget_80D $addr $r_ISENSE_CONFIG w)"          
+            echo "IOUT_AVG_UC_FAULT_LIMIT,  $($i2cget_80D $addr $r_IOUT_AVG_UC_FAULT_LIMIT w)"
+            echo "VOUT_COMMAND ,            $($i2cget_80D $addr $r_VOUT_COMMAND w)"      
+            echo "VOUT_MAX ,                $($i2cget_80D $addr $r_VOUT_MAX w)"               
+            echo "VOUT_UV_FAULT_LIMIT ,     $($i2cget_80D $addr $r_VOUT_UV_FAULT_LIMIT w)"
+            echo "VOUT_MARGIN_HIGH ,        $($i2cget_80D $addr $r_VOUT_MARGIN_HIGH w)"
+            echo "VOUT_MARGIN_LOW ,         $($i2cget_80D $addr $r_VOUT_MARGIN_LOW w)"
+            echo "POWER_GOOD_ON ,           $($i2cget_80D $addr $r_POWER_GOOD_ON w)"
+            echo "INTERLEAVE ,              $($i2cget_80D $addr $r_INTERLEAVE w)"
+            echo "VOUT_DROOP ,              $($i2cget_80D $addr $r_VOUT_DROOP w)"
+            echo "MULTI_PHASE_RAMP_GAIN,    $($i2cget_80D $addr $r_MULTI_PHASE_RAMP_GAIN)"
         done
     done
 
 elif [ $option -eq 2 ]
 then
-    echo "Implementing is ongoing"
-else
-    echo "option must be 1 for reading and 2 for setting the config, your option is $option"
-    helpFunction
+    echo "command is as below"
+    for i in {0..1}
+    do
+        if [ $i -eq 0 ]
+        then
+            addr=$master
+        elif [ $i -eq 1 ]
+        then
+            addr=$slave
+        fi
+        for j in {0..1}
+        do
+            echo "$i2cset_80D $addr $r_PAGE $j"
+            echo "$i2cset_80D $addr $r_FREQUENCY_SWITCH $h_FREQUENCY_SWITCH w"       
+            echo "$i2cset_80D $addr $r_VIN_UV_FAULT_LIMIT $h_VIN_UV_FAULT_LIMIT w"     
+            echo "$i2cset_80D $addr $r_VIN_UV_WARN_LIMIT $h_VIN_UV_WARN_LIMIT w"      
+            echo "$i2cset_80D $addr $r_VIN_OV_FAULT_LIMIT $h_VIN_OV_FAULT_LIMIT w"     
+            echo "$i2cset_80D $addr $r_VIN_OV_WARN_LIMIT $h_VIN_OV_WARN_LIMIT w"      
+            echo "$i2cset_80D $addr $r_OT_FAULT_LIMIT $h_OT_FAULT_LIMIT w"         
+            echo "$i2cset_80D $addr $r_OT_WARN_LIMIT $h_OT_WARN_LIMIT w"          
+            echo "$i2cset_80D $addr $r_VOUT_OV_FAULT_LIMIT $h_VOUT_OV_FAULT_LIMIT w"    
+            echo "$i2cset_80D $addr $r_IOUT_AVG_OC_FAULT_LIMIT $h_IOUT_AVG_OC_FAULT_LIMIT w"
+            echo "$i2cset_80D $addr $r_IOUT_OC_FAULT_LIMIT $h_IOUT_OC_FAULT_LIMIT w"    
+            echo "$i2cset_80D $addr $r_IOUT_CAL_GAIN $h_IOUT_CAL_GAIN w"          
+            echo "$i2cset_80D $addr $r_IOUT_CAL_OFFSET $h_IOUT_CAL_OFFSET w"        
+            echo "$i2cset_80D $addr $r_IOUT_UC_FAULT_LIMIT $h_IOUT_UC_FAULT_LIMIT w"    
+            echo "$i2cset_80D $addr $r_ISENSE_CONFIG $h_ISENSE_CONFIG w"          
+            echo "$i2cset_80D $addr $r_IOUT_AVG_UC_FAULT_LIMIT $h_IOUT_AVG_UC_FAULT_LIMIT w"
+            echo "$i2cset_80D $addr $r_VOUT_COMMAND $h_VOUT_COMMAND w"      
+            echo "$i2cset_80D $addr $r_VOUT_MAX $h_VOUT_MAX w"               
+            echo "$i2cset_80D $addr $r_VOUT_UV_FAULT_LIMIT $h_VOUT_UV_FAULT_LIMIT w"
+            echo "$i2cset_80D $addr $r_VOUT_MARGIN_HIGH $h_VOUT_MARGIN_HIGH w"
+            echo "$i2cset_80D $addr $r_VOUT_MARGIN_LOW $h_VOUT_MARGIN_LOW w"
+            echo "$i2cset_80D $addr $r_POWER_GOOD_ON $h_POWER_GOOD_ON w"
+            echo "$i2cset_80D $addr $r_INTERLEAVE $h_INTERLEAVE w"
+            echo "$i2cset_80D $addr $r_VOUT_DROOP $h_VOUT_DROOP w"
+            echo "$i2cset_80D $addr $r_MULTI_PHASE_RAMP_GAIN $h_MULTI_PHASE_RAMP_GAIN"
+        done
+    done
 fi
 
 
-
-
+if [ $reset -eq 1 ]
+then
+    for i in {0..1}
+    do
+        if [ $i -eq 0 ]
+        then
+            addr=$master
+        elif [ $i -eq 1 ]
+        then
+            addr=$slave
+        fi
+        #for j in {0..1}
+        #do
+            #$i2cset_80D $addr $r_PAGE $j
+            $i2cset_80D $addr $r_RESTORE_FACTORY
+	    sleep 5 
+            #$i2cset_80D $addr $r_RESTORE_USER_ALL
+            $i2cset_80D $addr $r_STORE_USER_ALL
+        #done
+    done
+fi
 
--- truck/shell/i2c_read_fan.sh
+++ truck/shell/i2c_read_fan.sh
@@ -5,7 +5,7 @@ helpFunction()
 {
     echo ""
     echo "Usage: $0 -f parameterB -s parameterC"
-    echo -e "\t-f Description of what fan#, must be 1, 2 or 3"
+    echo -e "\t-f Description of what fan#, must be 1, 2, 3. 4 is for all fans"
     echo -e "\t-s Sescription of what speed, must be in the range 0~255"
     exit 1 # Exit script after printing help
 }
@@ -20,7 +20,7 @@ do
 done
 
 # Print helpFunction in case parameter are empty
-if [ -z "$fan_sel" ] || [ -z "$fan_speed" ] || [ $fan_sel -lt 1 ] || [ $fan_sel -gt 3 ] || [ $fan_speed -lt 1 ] || [ $fan_speed -gt 255 ]
+if [ -z "$fan_sel" ] || [ -z "$fan_speed" ] || [ $fan_sel -lt 1 ] || [ $fan_sel -gt 4 ] || [ $fan_speed -lt 1 ] || [ $fan_speed -gt 255 ]
 then
     echo "Some or all of the parameters are empty";
     helpFunction
@@ -30,7 +30,7 @@ echo "fan selection is: $fan_sel"
 echo "fan speed is: $fan_speed"
 
 
-i2cset_fan_ctrl="i2cset -f -y 1 0x2e" #ADT7476 slave address is 0x23
+i2cset_fan_ctrl="i2cset -f -y 1 0x2e" #ADT7476 slave address is 0x2e
 i2cget_fan_ctrl="i2cget -f -y 1 0x2e"
 GPIO_mux="0x7C"     #bit[4] set the VID as GPO function
 GPIO_set="0x43"     #bit[0] is relay: 1 active, bit[1] is alarm led: 0 active, bit[2] is sys led: 0 active
@@ -75,11 +75,6 @@ val=$($i2cget_fan_ctrl $GPIO_set)
 echo "GPIO_set read value, $val"
 
 
-val=$($i2cget_fan_ctrl $PWM3_manual)
-val=$(($val | 0xe0))
-$i2cset_fan_ctrl $PWM3_manual $val
-val=$($i2cget_fan_ctrl $PWM3_manual)
-echo "PWM3_manual read value, $val"
 
 val=$($i2cget_fan_ctrl $PWM1_manual)
 val=$(($val | 0xe0))
@@ -92,6 +87,12 @@ val=$(($val | 0xe0))
 $i2cset_fan_ctrl $PWM2_manual $val
 val=$($i2cget_fan_ctrl $PWM2_manual)
 echo "PWM2_manual read value, $val"
+
+val=$($i2cget_fan_ctrl $PWM3_manual)
+val=$(($val | 0xe0))
+$i2cset_fan_ctrl $PWM3_manual $val
+val=$($i2cget_fan_ctrl $PWM3_manual)
+echo "PWM3_manual read value, $val"
 
 val=$($i2cget_fan_ctrl $PWM_STRT)
 val=$(($val | 0x01))
@@ -112,7 +113,6 @@ $i2cset_fan_ctrl $PWM_FPWM $val
 val=$($i2cget_fan_ctrl $PWM_FPWM)
 echo "PWM_FPWM read value, $val"
 
-
 if [ $fan_sel = "1" ] 
 then
     $i2cset_fan_ctrl $PWM1_val $fan_speed
@@ -121,6 +121,11 @@ then
     $i2cset_fan_ctrl $PWM2_val $fan_speed
 elif [ $fan_sel = "3" ]
 then
+    $i2cset_fan_ctrl $PWM3_val $fan_speed
+elif [ $fan_sel = "4" ]
+then
+    $i2cset_fan_ctrl $PWM1_val $fan_speed
+    $i2cset_fan_ctrl $PWM2_val $fan_speed
     $i2cset_fan_ctrl $PWM3_val $fan_speed
 fi
 
@@ -131,26 +136,59 @@ do
     then
         low_byte=$($i2cget_fan_ctrl $FAN_TACH1_L)
         high_byte=$($i2cget_fan_ctrl $FAN_TACH1_H)
+        low_byte=$(($low_byte))
+        high_byte=($(($high_byte)))
+        high_byte=`expr $high_byte \* 256`
+        tach_val=`expr $high_byte \+ $low_byte`
+        fan_r_speed=`expr $OSC_CLK \* $min_sec / $tach_val`
+        echo "$fan_sel,$fan_speed, $fan_r_speed"
     elif [ $fan_sel = "2" ] 
     then
         low_byte=$($i2cget_fan_ctrl $FAN_TACH2_L)
         high_byte=$($i2cget_fan_ctrl $FAN_TACH2_H)
+        low_byte=$(($low_byte))
+        high_byte=($(($high_byte)))
+        high_byte=`expr $high_byte \* 256`
+        tach_val=`expr $high_byte \+ $low_byte`
+        fan_r_speed=`expr $OSC_CLK \* $min_sec / $tach_val`
+        echo "$fan_sel,$fan_speed, $fan_r_speed"
     elif [ $fan_sel = "3" ]
     then
         low_byte=$($i2cget_fan_ctrl $FAN_TACH3_L)
         high_byte=$($i2cget_fan_ctrl $FAN_TACH3_H)
+        low_byte=$(($low_byte))
+        high_byte=($(($high_byte)))
+        high_byte=`expr $high_byte \* 256`
+        tach_val=`expr $high_byte \+ $low_byte`
+        fan_r_speed=`expr $OSC_CLK \* $min_sec / $tach_val`
+        echo "$fan_sel,$fan_speed, $fan_r_speed"
+    elif [ $fan_sel = "4" ]
+    then
+        low_byte1=$($i2cget_fan_ctrl $FAN_TACH1_L)
+        high_byte1=$($i2cget_fan_ctrl $FAN_TACH1_H)
+        low_byte2=$($i2cget_fan_ctrl $FAN_TACH2_L)
+        high_byte2=$($i2cget_fan_ctrl $FAN_TACH2_H)
+        low_byte3=$($i2cget_fan_ctrl $FAN_TACH3_L)
+        high_byte3=$($i2cget_fan_ctrl $FAN_TACH3_H)
+        low_byte1=$(($low_byte1))
+        low_byte2=$(($low_byte2))
+        low_byte3=$(($low_byte3))
+        high_byte1=($(($high_byte1)))
+        high_byte2=($(($high_byte2)))
+        high_byte3=($(($high_byte3)))
+        high_byte1=`expr $high_byte1 \* 256`
+        high_byte2=`expr $high_byte2 \* 256`
+        high_byte3=`expr $high_byte3 \* 256`
+        tach_val1=`expr $high_byte1 \+ $low_byte1`
+        tach_val2=`expr $high_byte2 \+ $low_byte2`
+        tach_val3=`expr $high_byte3 \+ $low_byte3`
+        fan_r_speed1=`expr $OSC_CLK \* $min_sec / $tach_val1`
+        fan_r_speed2=`expr $OSC_CLK \* $min_sec / $tach_val2`
+        fan_r_speed3=`expr $OSC_CLK \* $min_sec / $tach_val3`
+        echo "fan1, $fan_r_speed1"
+        echo "fan2, $fan_r_speed2"
+        echo "fan3, $fan_r_speed3"
     fi
-    #echo "low_byte,$low_byte"
-    #echo "high_byte,$high_byte"
-    low_byte=$(($low_byte))
-    high_byte=($(($high_byte)))
-    high_byte=`expr $high_byte \* 256`
-    #echo "low_byte,$low_byte"
-    #echo "high_byte,$high_byte"
-    tach_val=`expr $high_byte \+ $low_byte`
-    fan_r_speed=`expr $OSC_CLK \* $min_sec / $tach_val`
-    #echo "tach_val,$tach_val"
-    echo "$fan_sel,$fan_speed, $fan_r_speed"
     sleep 2
 done
 
